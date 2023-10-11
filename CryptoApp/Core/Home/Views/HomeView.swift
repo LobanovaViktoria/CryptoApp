fe80fdef806196a14508bf9dde54635e978d729e
Lobanova Viktoria <va_klevtsova@mail.ru>
2023-10-11 11:01:14 +0300

    Sorting List Data with MVVM and animations

--- CryptoApp/Core/Home/Views/HomeView.swift
+++ CryptoApp/Core/Home/Views/HomeView.swift
@@ -104,12 +104,42 @@ extension HomeView {
     
     private var columnTitle: some View {
         HStack {
-            Text("Coin")
+            HStack(spacing: 4) {
+                Text("Coin")
+                Image(systemName: "chevron.down")
+                    .opacity((vm.sortOption == .rank || vm.sortOption == .rankReversed) ? 1.0 : 0.0)
+                    .rotationEffect(Angle(degrees: vm.sortOption == .rank ? 0: 180))
+            }
+            .onTapGesture {
+                withAnimation {
+                    vm.sortOption = vm.sortOption == .rank ? .rankReversed : .rank
+                }
+            }
             Spacer()
             if showPortfolio {
-                Text("Holdings")
+                HStack(spacing: 4) {
+                    Text("Holdings")
+                    Image(systemName: "chevron.down")
+                        .opacity((vm.sortOption == .holdings || vm.sortOption == .holdingsReverced) ? 1.0 : 0.0)
+                        .rotationEffect(Angle(degrees: vm.sortOption == .holdings ? 0: 180))
+                }
+                .onTapGesture {
+                    withAnimation {
+                        vm.sortOption = vm.sortOption == .holdings ? .holdingsReverced : .holdings
+                    }
+                }
             }
-            Text("Price")
+            HStack(spacing: 4) {
+                Text("Price")
+                Image(systemName: "chevron.down")
+                    .opacity((vm.sortOption == .price || vm.sortOption == .priceReverced) ? 1.0 : 0.0)
+                    .rotationEffect(Angle(degrees: vm.sortOption == .price ? 0: 180))
+            }
+            .onTapGesture {
+                withAnimation {
+                    vm.sortOption = vm.sortOption == .price ? .priceReverced : .price
+                }
+            }
                 .frame(width: UIScreen.main.bounds.width / 3.5, alignment: .trailing)
             Button {
                 withAnimation(.linear(duration: 2.0)) {
